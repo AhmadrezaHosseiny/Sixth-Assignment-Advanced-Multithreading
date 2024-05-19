8670c7ff7ec3202c50eb5d06b5bd619376fbec93
AhmadrezaHosseiny <a.r.hosseiny1384@gmail.com>
2024-05-19 22:32:47 +0330

    done

--- src/main/java/sbu/cs/Semaphore/Resource.java
+++ src/main/java/sbu/cs/Semaphore/Resource.java
@@ -1,12 +1,22 @@
 package sbu.cs.Semaphore;
 
+import java.util.concurrent.Semaphore;
 public class Resource {
-
+    private static Semaphore semaphore = new Semaphore(2);
     public static void accessResource() {
         try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
+            System.out.println("Requesting in: " + Thread.currentThread().getName());
+            semaphore.acquire();
+            System.out.println("Running in: " + Thread.currentThread().getName());
+            Thread.sleep(500);
+        }
+
+        catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        System.out.println("Finished: " + Thread.currentThread().getName());
+
+        semaphore.release();
     }
 }
